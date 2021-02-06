115bad3352bac1ce50c82869754675fcf1aafb3e
Lance Barger <david.barger@cbre.com>
2021-02-06 07:38:56 -0600

    subnet

--- FlightLog.Terraform/mod-vm/main.tf
+++ FlightLog.Terraform/mod-vm/main.tf
@@ -1,3 +1,9 @@
-resource "aws_vpc" "dev" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "snet" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
 }
