fc8c60d47ccf9b615fe24fb77affd2d60d649831
Lance Barger <david.barger@cbre.com>
2021-02-06 09:38:36 -0600

    public ingress

--- FlightLog.Terraform/mod-vm/main.tf
+++ FlightLog.Terraform/mod-vm/main.tf
@@ -1,11 +1,19 @@
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
 }
 
 resource "aws_subnet" "snet" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+
+  map_public_ip_on_launch = true
+  depends_on = [aws_internet_gateway.gw]
 }
 
 resource "aws_network_interface" "nic" {
