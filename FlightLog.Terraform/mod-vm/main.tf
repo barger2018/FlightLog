4933c14dfcbe4b1480621d01d39749a18fb23d1f
Lance Barger <david.barger@cbre.com>
2021-02-06 14:47:53 -0600

    ssh

--- FlightLog.Terraform/mod-vm/main.tf
+++ FlightLog.Terraform/mod-vm/main.tf
@@ -1,25 +1,25 @@
-# resource "aws_vpc" "vpc" {
-#   cidr_block = "10.0.0.0/16"
-#   enable_dns_hostnames = true
-# }
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+}
 
-# resource "aws_internet_gateway" "gw" {
-#   vpc_id = aws_vpc.vpc.id
-# }
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.default.id
+}
 
-# resource "aws_subnet" "snet" {
-#   vpc_id            = aws_vpc.vpc.id
-#   cidr_block        = "10.0.1.0/24"
-#   availability_zone = "us-east-1a"
+resource "aws_subnet" "snet" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
-#   map_public_ip_on_launch = true
-#   depends_on = [aws_internet_gateway.gw]
-# }
+  map_public_ip_on_launch = true
+  depends_on = [aws_internet_gateway.gw]
+}
 
-# resource "aws_network_interface" "nic" {
-#   subnet_id   = aws_subnet.snet.id
-#   private_ips = ["10.0.1.100"]
-# }
+resource "aws_network_interface" "nic" {
+  subnet_id   = aws_subnet.snet.id
+  private_ips = ["10.0.1.100"]
+}
 
 resource "aws_key_pair" "aws" {
   key_name   = "aws"
--- FlightLog.Terraform/mod-vm/outputs.tf
+++ FlightLog.Terraform/mod-vm/outputs.tf
@@ -1,7 +1,7 @@
-# output vpc-id {
-#     value = aws_vpc.vpc.id
-# }
+output vpc-id {
+    value = aws_vpc.vpc.id
+}
 
-# output main-cidr-group {
-#     value = "10.0.0.0/16"
-# }
+output main-cidr-group {
+    value = "10.0.0.0/16"
+}
