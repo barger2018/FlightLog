f048693cbeed5f0a340ae9c4e46858352d62be6c
Lance Barger <david.barger@cbre.com>
2021-02-06 08:31:03 -0600

    vm

--- FlightLog.Terraform/mod-vm/main.tf
+++ FlightLog.Terraform/mod-vm/main.tf
@@ -12,3 +12,17 @@ resource "aws_network_interface" "nic" {
   subnet_id   = aws_subnet.snet.id
   private_ips = ["10.0.1.100"]
 }
+
+resource "aws_instance" "vm" {
+  ami           = "ami-047a51fa27710816e" # us-east-1
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.nic.id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+}
