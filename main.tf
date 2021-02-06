77d812513c8329f96e30e0872c8cde5c171cbec1
Lance Barger <david.barger@cbre.com>
2021-02-06 06:53:51 -0600

    use vs code

--- main.tf
+++ main.tf
@@ -6,12 +6,12 @@ terraform {
       name = "FlightLog"
     }
   }
+}
 
-  provider "aws" {
+provider "aws" {
     region = "us-east-1"
   }
 
-  resource "aws_vpc" "example" {
-    cidr_block = "10.0.0.0/16"
-  }
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
