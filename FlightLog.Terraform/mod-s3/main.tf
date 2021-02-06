b127d6b29b3214de59d19a4caaf1358e7bfcf4bd
Lance Barger <david.barger@cbre.com>
2021-02-06 08:53:55 -0600

    s3

--- FlightLog.Terraform/mod-s3/main.tf
+++ FlightLog.Terraform/mod-s3/main.tf
@@ -1,12 +1,12 @@
 resource "aws_s3_bucket" "lb-logs" {
   bucket = "lb-bucket"
-  acl    = "public"
+  acl    = "public-read-write"
 
 }
 
 resource "aws_s3_bucket" "build-logs" {
   bucket = "build-bucket"
-  acl    = "public"
+  acl    = "public-read-write"
 
 }
 
