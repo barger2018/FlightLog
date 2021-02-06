60505512a0364e019a6213d7744c357669671f73
Lance Barger <david.barger@cbre.com>
2021-02-06 07:45:30 -0600

    nic

--- FlightLog.Terraform/mod-vm/main.tf
+++ FlightLog.Terraform/mod-vm/main.tf
@@ -7,3 +7,8 @@ resource "aws_subnet" "snet" {
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 }
+
+resource "aws_network_interface" "nic" {
+  subnet_id   = aws_subnet.snet.id
+  private_ips = ["10.0.1.100"]
+}
