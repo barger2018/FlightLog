4978cffa4768d053b828b286d50634758885c639
Lance Barger <david.barger@cbre.com>
2021-02-07 10:21:02 -0600

    private s3

--- FlightLog.Terraform/mod-vm/main.tf
+++ FlightLog.Terraform/mod-vm/main.tf
@@ -98,7 +98,10 @@ sudo -i
 apt-get update
 apt-get -y install nginx
 service nginx start
-apt install aws-cli
+apt-get -y install unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
 EOF
 }
 
