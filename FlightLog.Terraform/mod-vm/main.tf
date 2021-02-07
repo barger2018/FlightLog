4fbf74037276f9c55b8680931f18c1e5ed4655dc
Lance Barger <david.barger@cbre.com>
2021-02-07 12:05:18 -0600

    private s3

--- FlightLog.Terraform/mod-vm/main.tf
+++ FlightLog.Terraform/mod-vm/main.tf
@@ -100,7 +100,7 @@ apt-get -y install unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
-
+echo $AWS_ACCESS_KEY_ID
 EOF
 
 }
