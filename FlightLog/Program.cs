82673b493b6a9726bf499a8a477663fcb69b4fb0
Lance Barger <david.barger@cbre.com>
2021-02-07 19:29:19 -0600

    Content

--- FlightLog/Program.cs
+++ FlightLog/Program.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
 
 namespace FlightLog
 {
@@ -20,8 +23,9 @@ namespace FlightLog
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                    webBuilder.UseKestrel();
+                    webBuilder.UseStartup<Startup>()
+                    .UseKestrel()
+                    .UseContentRoot(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
                 });
     }
 }
